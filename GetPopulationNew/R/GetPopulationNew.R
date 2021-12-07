e213591da6cbd94398a00afce3dc9852e271cbf1
romanceline <48318414+romanceline@users.noreply.github.com>
2021-12-07 17:45:21 +0100

    Update GetPopulationNew.R
    More stable feature service

--- GetPopulationNew/R/GetPopulationNew.R
+++ GetPopulationNew/R/GetPopulationNew.R
@@ -6,13 +6,14 @@
 
 GetPopulationNew<-function(){
   url <- parse_url("https://services.arcgis.com/5T5nSi527N4F7luB/ArcGIS/rest/services")
-  url$path <- paste(url$path, "EURO_COVID19_ADM0_Cases/FeatureServer/0/query", sep = "/")
+  url$path <- paste(url$path, "EURO_COVID_ADM0/FeatureServer/0/query", sep = "/")
   url$query <- list(where = '1=1',
                     outFields = "*",
-                    returnGeometry = "true",
+                    returnGeometry = "false",
                     f = "geojson")
   request <- build_url(url)
-  PopulationData <- st_read(request) %>% st_drop_geometry() %>% 
+  PopulationData <- st_read(request) %>% 
+    #%>% st_drop_geometry() %>% 
     select(ADM0_NAME,WHO_CODE,Population=CENTER_LAT) %>%
     mutate(ADM0_NAME=str_to_title(ADM0_NAME))
   
